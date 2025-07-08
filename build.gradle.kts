ff01e7e7e57876e589537a8ddbbef219cdcdae2d
Tyler Bertrand <tbertrand@gradle.com>
2025-07-08 13:05:52 -0500

    Upgrade to latest io.freefair.lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import io.freefair.gradle.plugins.lombok.tasks.LombokConfig
 
 plugins {
     id("java-library")
-    id("io.freefair.lombok") version "8.6"
+    id("io.freefair.lombok") version "8.14"
 }
 
 repositories {
